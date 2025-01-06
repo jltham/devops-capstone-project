5623750bdde7905ed0f4b0c36ccaec7b0a14e652
jltham <jltham18@gmail.com>
2025-01-06 11:08:19 -0500

    Fix linting and nosetests

--- service/routes.py
+++ service/routes.py
@@ -42,7 +42,7 @@ def index():
 def create_accounts():
     """
     Creates an Account
-    This endpoint will create an Account 
+    This endpoint will create an Account
     based the data in the body that is posted
     """
     app.logger.info("Request to create an Account")
@@ -52,12 +52,16 @@ def create_accounts():
     account.create()
     message = account.serialize()
     # Uncomment once get_accounts has been implemented
-    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
+    # location_url = url_for(
+    #     "get_accounts",
+    #     account_id=account.id,
+    #     _external=True)
     location_url = "/"  # Remove once get_accounts has been implemented
 
-        jsonify(message),
-        
-        ##################################################################
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
+##################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
 
@@ -93,7 +97,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(
-            status.HTTP_404_NOT_FOUND, 
+            status.HTTP_404_NOT_FOUND,
             f"Account with id [{account_id}] could not be found."
         )
     return account.serialize(), status.HTTP_200_OK
@@ -115,7 +119,7 @@ def update_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(
-            status.HTTP_404_NOT_FOUND, 
+            status.HTTP_404_NOT_FOUND,
             f"Account with id [{account_id}] could not be found."
         )
     account.deserialize(request.get_json())
@@ -133,7 +137,7 @@ def update_accounts(account_id):
 def delete_accounts(account_id):
     """
     Delete an Account
-    This endpoint will delete an Account based on the account_id 
+    This endpoint will delete an Account based on the account_id
     that is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
